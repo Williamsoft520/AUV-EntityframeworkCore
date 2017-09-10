1955472dc3bf5dded096ae7d5c0db6254005d80a
周大侠 <williamsoft520@126.com>
2017-09-10 22:14:57 +0800

    init

---
+++ sample/AUV.EntityframeworkCore.WebSample/Samples/SampleDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AUV.EntityframeworkCore.WebSample
+{
+    public class SampleDbContext:DbContext        
+    {
+        public SampleDbContext(DbContextOptions<SampleDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ sample/AUV.EntityframeworkCore.WebSample/Samples/User.cs
@@ -0,0 +1,16 @@
+﻿using AUV.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AUV.EntityframeworkCore.WebSample
+{
+    [Table("Users")]
+    public class User : IIdentityEntity
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required,StringLength(20)]
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
